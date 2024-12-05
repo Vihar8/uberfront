baf97f30b2a6f599d55db1e9510efc2dafcefeb5
Vihar8 <168709645+Vihar8@users.noreply.github.com>
2024-12-05 22:36:58 +0530

    socket

--- src/context/SocketContext.jsx
+++ src/context/SocketContext.jsx
@@ -32,19 +32,62 @@
 
 
 
-import React, { createContext, useEffect, useState } from 'react';
+// import React, { createContext, useEffect, useState } from 'react';
+// import { io } from 'socket.io-client';
+
+// export const SocketContext = createContext();
+
+// const socket = io(`${import.meta.env.VITE_BASE_URL}`,{
+//     transports: ['websocket']
+// }); // Replace with your server URL
+
+// const SocketProvider = ({ children }) => {
+//     const [ride, setRide] = useState(null);
+//     const [ridePopupVisible, setRidePopupVisible] = useState(false);
+
+//     useEffect(() => {
+//         // Basic connection logic
+//         socket.on('connect', () => {
+//             console.log('Connected to server');
+//         });
+
+//         socket.on('disconnect', () => {
+//             console.log('Disconnected from server');
+//         });
+
+//         socket.on('new-ride', (data) => {
+//             console.log('New ride data received:', data);
+//             setRide(data);
+//             setRidePopupVisible(true);
+//         });
+
+//         return () => {
+//             socket.off('new-ride'); // Clean up on unmount
+//         };
+//     }, []);
+
+//     return (
+//         <SocketContext.Provider value={{ socket, ride, setRide, ridePopupVisible, setRidePopupVisible }}>
+//             {children}
+//         </SocketContext.Provider>
+//     );
+// };
+
+// export default SocketProvider;
+
+
+
+
+
+
+import React, { createContext, useEffect } from 'react';
 import { io } from 'socket.io-client';
 
 export const SocketContext = createContext();
 
-const socket = io(`${import.meta.env.VITE_BASE_URL}`,{
-    transports: ['websocket']
-}); // Replace with your server URL
+const socket = io(`${import.meta.env.VITE_BASE_URL}`); // Replace with your server URL
 
 const SocketProvider = ({ children }) => {
-    const [ride, setRide] = useState(null);
-    const [ridePopupVisible, setRidePopupVisible] = useState(false);
-
     useEffect(() => {
         // Basic connection logic
         socket.on('connect', () => {
@@ -55,19 +98,12 @@ const SocketProvider = ({ children }) => {
             console.log('Disconnected from server');
         });
 
-        socket.on('new-ride', (data) => {
-            console.log('New ride data received:', data);
-            setRide(data);
-            setRidePopupVisible(true);
-        });
-
-        return () => {
-            socket.off('new-ride'); // Clean up on unmount
-        };
     }, []);
 
+
+
     return (
-        <SocketContext.Provider value={{ socket, ride, setRide, ridePopupVisible, setRidePopupVisible }}>
+        <SocketContext.Provider value={{ socket }}>
             {children}
         </SocketContext.Provider>
     );
