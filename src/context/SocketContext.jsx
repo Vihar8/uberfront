c87f0be5dee8e0c8458d31a7d4437e323ab3fc78
Vihar8 <168709645+Vihar8@users.noreply.github.com>
2024-12-05 22:31:10 +0530

    change socket

--- src/context/SocketContext.jsx
+++ src/context/SocketContext.jsx
@@ -37,7 +37,9 @@ import { io } from 'socket.io-client';
 
 export const SocketContext = createContext();
 
-const socket = io(`${import.meta.env.VITE_BASE_URL}`); // Replace with your server URL
+const socket = io(`${import.meta.env.VITE_BASE_URL}`,{
+    transports: ['websocket']
+}); // Replace with your server URL
 
 const SocketProvider = ({ children }) => {
     const [ride, setRide] = useState(null);
